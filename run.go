13b6cb6280de8818e6d275c1721d594216ed43bd
AaravSibbal <106462208+AaravSibbal@users.noreply.github.com>
2024-03-19 23:32:32 -0400

    run.go is completed now

--- run.go
+++ run.go
@@ -2,16 +2,16 @@ package main
 
 import "os"
 
-const UNEXPECTED_BEHAVIOUR = "Unexpected behaviour, typical skill issues\nrun 'zippy help' to do better" 
-
+const UNEXPECTED_BEHAVIOUR_ERR = "Unexpected behaviour, typical skill issues\nrun 'zippy help' to do better" 
+const INPUT_FILE_ERR = "Input file name not given"
 func (app *application) run(){
 	len := len(os.Args)
 	
 	switch len{
 		case 0:
-			app.errlog.Fatalln(UNEXPECTED_BEHAVIOUR)
+			app.errlog.Fatalln(UNEXPECTED_BEHAVIOUR_ERR)
 		case 1:
-			app.zipFile(app.input, app.outputFile)
+			app.zipDirectory(app.input, app.outputFile)
 			return
 		case 2:
 			option := os.Args[1]
@@ -22,11 +22,71 @@ func (app *application) run(){
 				case "-d":
 					app.zipDirectory(app.input, app.outputFile)
 				case "-f":
-					app.errlog.Fatalln("Input file name not given")
+					app.errlog.Fatalln(INPUT_FILE_ERR)
 				default:
-					app.errlog.Fatalln(UNEXPECTED_BEHAVIOUR)
+					app.errlog.Fatalln(UNEXPECTED_BEHAVIOUR_ERR)
 			}
 		case 3:
-				
+			// That is zippy [option] input
+			option := os.Args[1]
+			input := os.Args[2]
+
+			switch option {
+				case "-d":
+					switch input{
+						case "":
+							app.zipDirectory(app.input, app.outputFile)
+						case ".":
+							app.zipDirectory(app.input, app.outputFile)
+						
+						default:
+							app.zipDirectory(input, app.outputFile)
+					}
+				case "-f":
+					switch input{
+
+						case "":
+							app.errlog.Fatalln(INPUT_FILE_ERR)
+						default:
+							app.zipFile(input, app.outputFile)
+					}
+				default:
+					app.errlog.Fatalln(UNEXPECTED_BEHAVIOUR_ERR)
+			}
+		case 4:
+
+			option := os.Args[1]
+			input := os.Args[2]
+			output := os.Args[3]
+			switch option{
+				case "-d":
+					if(input == "." && output == "."){
+						app.zipDirectory(input, app.outputFile)
+					} else if(input == "." && output != "."){
+						app.zipDirectory(input, output)
+					} else if (input != "." && output != "."){
+						app.zipDirectory(input, output)
+					} else {
+						app.errlog.Fatalln(UNEXPECTED_BEHAVIOUR_ERR)
+					}
+
+					
+				case "-f":
+					if(input == "." && output == "."){
+						app.errlog.Fatalln(INPUT_FILE_ERR)
+					} else if (input != "." && output != "."){
+						app.zipFile(input, app.outputFile)
+					}else {
+						app.errlog.Fatalln(UNEXPECTED_BEHAVIOUR_ERR)
+					}
+				default:
+					app.errlog.Fatalln(UNEXPECTED_BEHAVIOUR_ERR)
+			}
+		default:
+			app.errlog.Fatalln(UNEXPECTED_BEHAVIOUR_ERR)			
 	}
 }
+
+// func twoArgs(input, output string){
+
+// }
--- zip.go
+++ zip.go
@@ -3,17 +3,20 @@ package main
 import "fmt"
 
 func (app *application) zipFile(input, outputFile string) {
+	// TODO: GET ZIP FILE WORKING
 	fmt.Println("add the zip file code here")
 
 	fmt.Println(input, outputFile)
 }
 
-func (app *application) zipDirectory(input, outFile string){
+func (app *application) zipDirectory(input, outputFile string){
 	fmt.Println("add the zip dir code here")
+	// TODO: GET THE ZIP DIRECTORY FUNCTION WORKING
+	fmt.Println(input, outputFile)
 }
 
+
+
 func (app *application) getDir(input string){
-	// .
-	// ../../
-	// somr
+	// TODO: GET THE GET DIR FUNCTION WORKING
 }
