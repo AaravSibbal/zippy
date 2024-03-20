a5b83b4112c6fd407034d8e66b1841e590871f91
AaravSibbal <106462208+AaravSibbal@users.noreply.github.com>
2024-03-19 20:22:40 -0400

    added help.txt or help option

--- main.go
+++ main.go
@@ -7,44 +7,56 @@ import (
 )
 
 /*
-	TODO: make the general file zip
-	TODO: make dir zip
-	TODO: make a nice man
-	TODO: add this to path
+TODO: make the general file zip
+TODO: make dir zip
+TODO: make a nice man
+TODO: add this to path
 */
 const EXPECTED_ARGS = 4
 
 type application struct {
-	errlog *log.Logger
+	input string
+	outputFile string
+	option string
+	errlog  *log.Logger
 	infoLog *log.Logger
 }
 
 func main() {
 
-	argsLen := len(os.Args)
-	WRONG_ARGS_LEN := fmt.Sprintf("Expected %d or got %d", EXPECTED_ARGS, argsLen)
+	option := "-d"
+	input := "."
+	outputFile := "."
 
+	argsLen := len(os.Args)
+	WRONG_ARGS_LEN := fmt.Sprintf("Expected %d got %d", EXPECTED_ARGS, argsLen)
 
 	errLog := log.New(os.Stdout, "Error: \t", log.Ldate|log.Ltime)
 	infoLog := log.New(os.Stdout, "Info: \t", log.Ldate|log.Ltime)
 
 	app := &application{
+		option: "-d",
+		input: ".",
+		outputFile: "output.zip",
 		infoLog: infoLog,
-		errlog: errLog,
-
+		errlog:  errLog,
 	}
-	if argsLen == 3 || argsLen > 3{
+
+	if argsLen < EXPECTED_ARGS {
 		errLog.Fatal(WRONG_ARGS_LEN)
 		return
 	}
-	
-	option := os.Args[1]
-	inputFile := os.Args[2]
-	outputFile := os.Args[3]
+	if os.Args[1] == "" {
 
-	if option == "-f"	 {
-		infoLog.Println("this option is for a file")
-		app.zipFile(inputFile, outputFile)
 	}
-	os.Exit(1)
+
+	option = os.Args[1]
+	input = os.Args[2]
+	outputFile = os.Args[3]
+
+	if option == "-f" {
+		infoLog.Println("this option is for a file")
+		app.zipFile(input, outputFile)
+	}
+	// os.Exit(1)
 }
